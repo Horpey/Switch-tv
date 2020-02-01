0c8c745f0b4b7af7d4c95735c93b6d066792699f
Horpey <adeniran1059@gmail.com>
2020-02-01 16:38:25 +0100

    New Slide Num

--- js/custom.js
+++ js/custom.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
     asNavFor: '.slider-nav'
   });
   $('.slider-nav').slick({
-    slidesToShow: 5,
+    slidesToShow: 4,
     slidesToScroll: 1,
     asNavFor: '.slider-for',
     dots: false,
